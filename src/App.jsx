0b07e1104fbcdae181b4149603f39af55eb6bade
saalcazar <alcazar.samuel@gmail.com>
2025-02-04 00:23:09 -0400

    Create forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,63 @@
+import { useState } from 'react'
 import './App.css'
+import { URL } from './helpers/config'
 
 function App () {
+  const [area, setArea] = useState('')
+
+  const submitArea = (e) => {
+    e.preventDefault()
+
+    const body = {
+      nameArea: e.target.area.value
+    }
+
+    fetch(`${URL}/areas`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setArea(data)
+      })
+      .catch((err) => console.log(err))
+  }
+
   return (
-    <main className='w-full h-screen flex justify-center items-center'>
-      <h1 className='text-3xl text-gray-400'>Hello Vite!</h1>
+    <main className='w-full flex flex-col justify-center items-center py-8'>
+      <h1 className='text-3xl text-gray-400'>ADMIN DATA</h1>
+      <section className='grid grid-cols-2 gap-4 mt-4'>
+        <section>
+          <form onSubmit={submitArea} className='w-[550px] border border-gray-600 px-4 py-2'>
+            <label className='text-gray-400'>
+              Área
+              <input type='text' name='area' id='' />
+            </label>
+          </form>
+          <form className='w-[550px] border border-gray-600 px-4 py-2'>
+            <label>
+              Input
+              <input type='text' name='' id='' />
+            </label>
+          </form>
+          <form className='w-[550px] border border-gray-600 px-4 py-2'>
+            <label>
+              Input
+              <input type='text' name='' id='' />
+            </label>
+          </form>
+        </section>
+        <form className='w-[550px] h-[500px] border border-gray-600 px-4 py-2'>
+          <label>
+            Input
+            <input type='text' name='' id='' />
+          </label>
+        </form>
+      </section>
     </main>
   )
 }
